14710aa2d61790b46c867325524a3d0397d93c8b
ArtCarreiro <arthur.carreirodev@gmail.com>
2026-04-23 21:21:35 -0300

    .

--- src/main/java/com/amc/api/config/CorsConfiguration.java
+++ src/main/java/com/amc/api/config/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
